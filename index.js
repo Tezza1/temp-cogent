c8af4c29b04f470db82ce433ad0c438fabed1f63
Tezza1 <tshenker@gmail.com>
2019-06-11 18:20:08 +0900

    added more shit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const upload = require('./upload');
-// const resize = require('./resize');
+const resize = require('./resize');
 
 const app = express();
 
